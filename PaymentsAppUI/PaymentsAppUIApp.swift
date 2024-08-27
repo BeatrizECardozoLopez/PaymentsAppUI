766f2fc587f83fb6c539fd430a0f131d6d8274ad
BeatrizECardozoLopez <beatrizecardozolopez@gmail.com>
2024-08-27 09:44:47 -0400

    Initial Commit

---
+++ PaymentsAppUI/PaymentsAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentsAppUIApp.swift
+//  PaymentsAppUI
+//
+//  Created by Beatriz Cardozo on 27/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PaymentsAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
